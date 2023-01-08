273d68356a4324e889938f08cda688215ccb877c
KMUS <KMUS1997@163.com>
2023-01-08 18:07:34 +0800

    Change the value from the cpp

--- dev/episoid/source/cpp20.cpp
+++ dev/episoid/source/cpp20.cpp
@@ -20,7 +20,7 @@ int main(){
 
 std::cout << "Hello HANG, Here is from C++ 20 with Cmake on Linux/Windows" << std::endl;
 
-std::cout << "666+888 的结果为: " <<add(666,888) << std::endl;
+std::cout << "666+888 的结果为: " <<add(1024,999) << std::endl;
 return 0;
 
 
